805bedd0ef725a9f705dd8f9752b0fe1c0652e12
CaptainCutlet <jonasurnezius@gmail.com>
2024-05-24 08:27:01 +0300

    Updated animation

--- src/Components/HowToBuy/HowToBuy.js
+++ src/Components/HowToBuy/HowToBuy.js
@@ -6,7 +6,6 @@ const HowToBuy = () => {
         <div className="how-to-buy">
             <h2>Ride the Rainbow Wave: Invest in <span className="rainbow_text_animated6">$RAINBOW</span></h2>
             <h3>Exposing the Glittering Mirage of Corporate Pride</h3>
-            <h4>Join the Revolution button leading to a sign-up or buy-in page</h4>
             <h2>How to Buy</h2>
             <ol>
                 <li>Create a base wallet and top it with some ETH.</li>
